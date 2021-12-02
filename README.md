0373736401a04f44f8ffc7db68392386b6b4bbfe
Matt Condon <mattgcondon@gmail.com>
2021-12-02 15:41:57 -0700

    feat: supports console.log output of ETH contributions to the safe

---
+++ README.md
@@ -0,0 +1,13 @@
+# safe-contribution-snapshot
+
+> Snapshots ETH contributions to a Gnosis Safe
+
+- [ ] snapshots from source block
+- [ ] resumable from block
+- [ ] merges / de-dupes from previous snapshot
+
+```
+ts-node index.ts [block]
+```
+
+- Gnosis Safe Creation Block: `13724221`
--- index.ts
+++ index.ts
@@ -1,4 +1,50 @@
-async function main() {}
+import { ethers } from 'ethers';
+import { config } from 'dotenv';
+import { formatEther } from '@ethersproject/units';
+
+config();
+
+const FREEROSSDAO_SAFE_ADDRESS = '0xc102d2544a7029f7BA04BeB133dEADaA57fDF6b4';
+const BLOCKS_PER_CHUNK = 50;
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_ENDPOINT as string);
+const safe = new ethers.Contract(
+  FREEROSSDAO_SAFE_ADDRESS,
+  [`event SafeReceived(address indexed sender, uint256 value)`],
+  provider,
+);
+
+async function main() {
+  const [, , _fromBlock, _toBlock] = process.argv;
+  const fromBlock = parseInt(_fromBlock);
+  const toBlock = _toBlock
+    ? parseInt(_toBlock)
+    : await provider.getBlockNumber().then((num) => num.toString());
+
+  console.log(`Snapshotting from ${fromBlock} to ${toBlock}`);
+  const filter = safe.filters.SafeReceived();
+
+  const handleContribution = (sender: string, value: ethers.BigNumber) => {
+    console.log(`${sender} sent ${formatEther(value)} ETH`);
+  };
+
+  for (let i = fromBlock; i <= toBlock; i = i + BLOCKS_PER_CHUNK) {
+    const fromChunkNumber = i;
+    const toChunkNumber = i + BLOCKS_PER_CHUNK - 1;
+
+    console.log(`checking in ${fromChunkNumber} => ${toChunkNumber}...`);
+
+    const events = await safe.queryFilter(filter, fromChunkNumber, toChunkNumber);
+    events.filter(Boolean).forEach((event: ethers.Event) => {
+      if (!event.args?.sender || !event.args?.value) {
+        console.log(`Invalid event??`, event);
+        return;
+      }
+
+      handleContribution(event.args.sender as string, event.args.value as ethers.BigNumber);
+    });
+  }
+}
 
 main()
   .then(() => {
