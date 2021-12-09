79057baf19ddf837900b1f689748bb18e759ca0c
Matt Condon <mattgcondon@gmail.com>
2021-12-09 13:25:28 -0400

    feat: cex overrides

--- index.ts
+++ index.ts
@@ -6,7 +6,29 @@ import { readFileSync, writeFileSync, existsSync } from 'fs';
 
 config();
 
+const CEX_OVERRIDES: Record<string, string> = {
+  // albatron9000#4542
+  '0xe3f21f1be6131aab64509c4ccf91807446194548866bbbef363db458e0d57252':
+    '0xE00Dc5eB53d376a50556f0039FDd3dA7dc83F06B',
+  // avax_szn#2301
+  '93d45d0c3c8acf3a1a10789fd2416d97a5419ec72d7ee625bccf884b306e9130':
+    '0x7AC258e14B6f580a9f2C556022898c813BaD2036',
+  // aliciakatz#6843
+  '0xcc458f154221c31f14b03772421837cce2969a3d38f81147927f32ef8ec9b85a':
+    '0x599Af7f3Eb2Af4f39A8174f1fab2cca09ff11a5d',
+  // jonathanbroly#4332 TODO
+  '0x073d9da34276075d7344df9f5b62e1785302b8b7da062efc62e2eec1b214eeb8':
+    '0x89C14066d9b643BFF11148ddBCc6c32F8E07C3FA',
+  // qasak#4459
+  '0x2b3772b50ffddc076c74105c787f44d7a0f009932a3814f9f4909a312935dff9':
+    '0xb6d84d05b7facFA94FAFB2f40d849B80A3f34FB7',
+  // billy_72#4443
+  '0xeb05dd124e0cbc16f2352fab345f55d475e51f88752777d2ebf530024edea2d8':
+    '0xbF4B0bcDcC7DC29AB19f89d08b3a139893CdCFE6',
+};
+
 const SAFE_DEPLOYED_IN_BLOCK = 13724221;
+const AUCTION_ENDED_IN_BLOCK = 13770208;
 const FREEROSSDAO_SAFE_ADDRESS = '0xc102d2544a7029f7BA04BeB133dEADaA57fDF6b4';
 const BLOCKS_PER_CHUNK = 100;
 const SNAPSHOT_FILENAME = 'snapshot.csv';
@@ -63,7 +85,7 @@ async function main() {
   console.log(`Snapshot has ${Object.keys(snapshot).length} entries.`);
 
   const fromBlock = getNextBlock();
-  const toBlock = await provider.getBlockNumber();
+  const toBlock = AUCTION_ENDED_IN_BLOCK;
 
   console.log(`Snapshotting from ${fromBlock} to ${toBlock}`);
   const filter = safe.filters.SafeReceived();
@@ -88,7 +110,18 @@ async function main() {
           return;
         }
 
-        handleContribution(event.args.sender as string, event.args.value as ethers.BigNumber);
+        const sender = CEX_OVERRIDES[event.transactionHash] ?? (event.args.sender as string);
+        const value = event.args.value as ethers.BigNumber;
+
+        if (CEX_OVERRIDES[event.transactionHash]) {
+          console.log(
+            `remapping tx ${event.transactionHash} for ${ethers.utils.formatEther(
+              value,
+            )} ETH from ${event.args.sender} to ${CEX_OVERRIDES[event.transactionHash]}`,
+          );
+        }
+
+        handleContribution(sender, value);
       });
 
       console.log(`setting next to ${toChunkNumber + 1}`);
