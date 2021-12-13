730242040c056770dfae8f6c32cbb7576d0fc2aa
Matthew Bunday <mkbunday@gmail.com>
2021-12-13 13:28:53 -0500

    Extract transactions

--- index.ts
+++ index.ts
@@ -4,48 +4,13 @@ import { formatEther } from '@ethersproject/units';
 import { parse, stringify } from 'csv/sync';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 
+import {
+  CEX_OVERRIDES,
+  EXCLUDE_TRANSACTIONS,
+  KNOWN_MISSING_TRANSACTIONS,
+} from './lib/transactions';
+
 config();
-
-const CEX_OVERRIDES: Record<string, string> = {
-  // albatron9000#4542
-  '0xe3f21f1be6131aab64509c4ccf91807446194548866bbbef363db458e0d57252':
-    '0xE00Dc5eB53d376a50556f0039FDd3dA7dc83F06B',
-  // avax_szn#2301
-  '93d45d0c3c8acf3a1a10789fd2416d97a5419ec72d7ee625bccf884b306e9130':
-    '0x7AC258e14B6f580a9f2C556022898c813BaD2036',
-  // aliciakatz#6843
-  '0xcc458f154221c31f14b03772421837cce2969a3d38f81147927f32ef8ec9b85a':
-    '0x599Af7f3Eb2Af4f39A8174f1fab2cca09ff11a5d',
-  // jonathanbroly#4332 TODO
-  '0x073d9da34276075d7344df9f5b62e1785302b8b7da062efc62e2eec1b214eeb8':
-    '0x89C14066d9b643BFF11148ddBCc6c32F8E07C3FA',
-  // qasak#4459
-  '0x2b3772b50ffddc076c74105c787f44d7a0f009932a3814f9f4909a312935dff9':
-    '0xb6d84d05b7facFA94FAFB2f40d849B80A3f34FB7',
-  // billy_72#4443
-  '0xeb05dd124e0cbc16f2352fab345f55d475e51f88752777d2ebf530024edea2d8':
-    '0xbF4B0bcDcC7DC29AB19f89d08b3a139893CdCFE6',
-  // vitozhang.eth#8367
-  '0x15a8bd82db8ab1f89735b3363450f58026bb1584e7945de7857ca6ea6dac1675':
-    '0x89E200fB309dfea8577bAa5aBD9268de00E27F7d',
-  // datwh0re#2006
-  '0xef8fdbd3aa246ee78d04637a7ab099bacea814c4122e13328945582595b2bc12':
-    '0xAeAE3050c45e4B013b08d532A842982609966945',
-  // jack005#6109
-  '0xfcf6af6008d668951dd7ba1c7fe875df959c1e769efa5ca96d907b60755a43d2':
-    '0x42b70Cd9BE0fba250f7838e0b006C1c3560C42D9',
-};
-
-// Exclude both the failed and successful auction refunds
-const EXCLUDE_TRANSACTIONS: Record<string, boolean> = {
-  '0xcfa584f6d072fd544a6f4e264eaededd4a5d3a0b842b3d3a6ff173f34682a698': true,
-  '0xad60df9a9df45d471f0a49062c9b6651cbc5de69e3964a4487337daa67104c9e': true,
-}
-
-const KNOWN_MISSING_TRANSACTIONS: Record<string, boolean> = {
-  '0x34c2c9aaafa12c317f4489ec2947650ce4213e3b90e3c2ba2267d2b1281650e9': true,
-  '0x3c39bc7e954109456b03a29c40c653a7e83f092ee4d2d272f34b6646a018cbe1': true,
-}
 
 const SAFE_DEPLOYED_IN_BLOCK = 13724221;
 const AUCTION_ENDED_IN_BLOCK = 13770208;
@@ -133,14 +98,14 @@ async function main() {
 
     if (CEX_OVERRIDES[event.transactionHash]) {
       console.log(
-        `remapping tx ${event.transactionHash} for ${ethers.utils.formatEther(
-          value,
-        )} ETH from ${event.args.sender} to ${CEX_OVERRIDES[event.transactionHash]}`,
+        `remapping tx ${event.transactionHash} for ${ethers.utils.formatEther(value)} ETH from ${
+          event.args.sender
+        } to ${CEX_OVERRIDES[event.transactionHash]}`,
       );
     }
 
     handleContribution(sender, value);
-  }
+  };
 
   for (let i = fromBlock; i <= toBlock; i = i + BLOCKS_PER_CHUNK) {
     const fromChunkNumber = i;
