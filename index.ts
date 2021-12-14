0a5bc3d3784f817ccbcb67064311a59097a99a2c
Matthew Bunday <mkbunday@gmail.com>
2021-12-13 19:02:53 -0500

    Solid state snapshotting

--- index.ts
+++ index.ts
@@ -16,63 +16,118 @@ import {
   BLOCKS_PER_CHUNK,
   SAFE_DEPLOYED_IN_BLOCK,
 } from './lib/const';
-import { readFromSnapshot, writeToSnapshot, getNextBlock, setNextBlock } from './lib/io';
+import { snapshotFilename, readSnapshot, writeSnapshot } from './lib/io';
 
 config();
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_ENDPOINT as string);
-const safe = new ethers.Contract(
-  FREEROSSDAO_SAFE_ADDRESS,
-  [`event SafeReceived(address indexed sender, uint256 value)`],
-  provider,
-);
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-async function main(startBlock: number, toBlock: number, chunkSize: number) {
-  const snapshot = readFromSnapshot();
-  console.log(`Snapshot has ${Object.keys(snapshot).length} entries.`);
+// async function main(startBlock: number, toBlock: number, chunkSize: number) {
+//   const snapshot = readFromSnapshot();
+//   console.log(`Snapshot has ${Object.keys(snapshot).length} entries.`);
 
-  const fromBlock = getNextBlock() || startBlock;
+//   const fromBlock = getNextBlock() || startBlock;
 
-  console.log(`Snapshotting from ${fromBlock} to ${toBlock}`);
+//   console.log(`Snapshotting from ${fromBlock} to ${toBlock}`);
+//   const filter = safe.filters.SafeReceived();
+
+//   const handleContribution = (sender: string, value: ethers.BigNumber) => {
+//     console.log(`${sender} sent ${formatEther(value)} ETH`);
+//     snapshot[sender] = (snapshot[sender] ?? ethers.BigNumber.from(0)).add(value);
+//   };
+
+//   const handleEvent = (event: ethers.Event) => {
+//     if (!event.args?.sender || !event.args?.value) {
+//       console.log(`Invalid event??`, event);
+//       return;
+//     }
+//     if (EXCLUDE_TRANSACTIONS[event.transactionHash]) {
+//       console.log('Exluding transaction', event.transactionHash);
+//       return;
+//     }
+//     if (KNOWN_MISSING_TRANSACTIONS[event.transactionHash]) {
+//       console.log('!!! FOUND KNOWN MISSING !!!', event.transactionHash);
+//     }
+
+//     const sender = CEX_OVERRIDES[event.transactionHash] ?? (event.args.sender as string);
+//     const value = event.args.value as ethers.BigNumber;
+
+//     if (CEX_OVERRIDES[event.transactionHash]) {
+//       console.log(
+//         `remapping tx ${event.transactionHash} for ${ethers.utils.formatEther(value)} ETH from ${
+//           event.args.sender
+//         } to ${CEX_OVERRIDES[event.transactionHash]}`,
+//       );
+//     }
+
+//     handleContribution(sender, value);
+//   };
+
+//   for (let i = fromBlock; i <= toBlock; i = i + chunkSize) {
+//     const fromChunkNumber = i;
+//     const toChunkNumber = Math.min(fromChunkNumber + chunkSize - 1, toBlock);
+
+//     console.log(`checking in ${fromChunkNumber} => ${toChunkNumber}...`);
+
+//     try {
+//       const events = await safe.queryFilter(filter, fromChunkNumber, toChunkNumber);
+//       console.log(`got ${events.length} events in this set of blocks`);
+//       events.filter(Boolean).forEach(handleEvent);
+
+//       console.log(`setting next to ${toChunkNumber + 1}`);
+//       setNextBlock(toChunkNumber + 1);
+
+//       await sleep(2000);
+//     } catch (error) {
+//       console.error(error);
+//       break;
+//     }
+//   }
+
+//   console.log('writing to snapshot.csv');
+//   writeToSnapshot(snapshot);
+// }
+
+async function fetchTransactions(
+  startBlock: number,
+  endBlock: number,
+  chunkSize: number,
+  contractAddress: string,
+) {
+  const safe = new ethers.Contract(
+    contractAddress,
+    [`event SafeReceived(address indexed sender, uint256 value)`],
+    provider,
+  );
   const filter = safe.filters.SafeReceived();
 
-  const handleContribution = (sender: string, value: ethers.BigNumber) => {
-    console.log(`${sender} sent ${formatEther(value)} ETH`);
-    snapshot[sender] = (snapshot[sender] ?? ethers.BigNumber.from(0)).add(value);
-  };
+  const filename = snapshotFilename(startBlock, endBlock, contractAddress);
+  const snapshot = readSnapshot(filename);
+  const fromBlock = snapshot.currentBlock || startBlock;
+
+  console.log(`Snapshoting from ${fromBlock} to ${endBlock}`);
 
   const handleEvent = (event: ethers.Event) => {
     if (!event.args?.sender || !event.args?.value) {
       console.log(`Invalid event??`, event);
       return;
     }
-    if (EXCLUDE_TRANSACTIONS[event.transactionHash]) {
-      console.log('Exluding transaction', event.transactionHash);
-      return;
-    }
-    if (KNOWN_MISSING_TRANSACTIONS[event.transactionHash]) {
-      console.log('!!! FOUND KNOWN MISSING !!!', event.transactionHash);
-    }
 
-    const sender = CEX_OVERRIDES[event.transactionHash] ?? (event.args.sender as string);
+    const sender = event.args.sender as string;
     const value = event.args.value as ethers.BigNumber;
 
-    if (CEX_OVERRIDES[event.transactionHash]) {
-      console.log(
-        `remapping tx ${event.transactionHash} for ${ethers.utils.formatEther(value)} ETH from ${
-          event.args.sender
-        } to ${CEX_OVERRIDES[event.transactionHash]}`,
-      );
-    }
-
-    handleContribution(sender, value);
+    snapshot.transactions[event.transactionHash] = {
+      hash: event.transactionHash,
+      sender,
+      value,
+    };
   };
 
-  for (let i = fromBlock; i <= toBlock; i = i + chunkSize) {
+  for (let i = fromBlock; i <= endBlock; i = i + chunkSize) {
     const fromChunkNumber = i;
-    const toChunkNumber = Math.min(fromChunkNumber + chunkSize - 1, toBlock);
+    const toChunkNumber = Math.min(fromChunkNumber + chunkSize - 1, endBlock);
 
     console.log(`checking in ${fromChunkNumber} => ${toChunkNumber}...`);
 
@@ -81,31 +136,33 @@ async function main(startBlock: number, toBlock: number, chunkSize: number) {
       console.log(`got ${events.length} events in this set of blocks`);
       events.filter(Boolean).forEach(handleEvent);
 
-      console.log(`setting next to ${toChunkNumber + 1}`);
-      setNextBlock(toChunkNumber + 1);
+      snapshot.currentBlock = toChunkNumber + 1;
+      console.log(`set next to ${toChunkNumber + 1}`);
 
-      await sleep(2000);
+      writeSnapshot(filename, snapshot);
+      await sleep(1400);
     } catch (error) {
       console.error(error);
       break;
     }
   }
-
-  console.log('writing to snapshot.csv');
-  writeToSnapshot(snapshot);
 }
+
+const parseDec = (n: string) => parseInt(n, 10);
 
 program.version('0.0.1');
 program
-  .option('-s, --start <block>', 'start block', parseInt, SAFE_DEPLOYED_IN_BLOCK)
-  .option('-e, --end <block>', 'end block', parseInt, AUCTION_ENDED_IN_BLOCK)
-  .option('--chunk <number>', 'blocks per chunk', parseInt, BLOCKS_PER_CHUNK);
+  .option('-s, --start <block>', 'start block', parseDec, SAFE_DEPLOYED_IN_BLOCK)
+  .option('-e, --end <block>', 'end block', parseDec, AUCTION_ENDED_IN_BLOCK)
+  .option('--chunk <number>', 'blocks per chunk', parseDec, BLOCKS_PER_CHUNK)
+  .option('-a, --address <address>', 'address', FREEROSSDAO_SAFE_ADDRESS);
 
 program.parse(process.argv);
 
 const options = program.opts();
 
-main(options.start, options.end, options.chunk)
+console.log(options);
+fetchTransactions(options.start, options.end, options.chunk, options.address)
   .then(() => {
     process.exit(0);
   })
--- lib/io.ts
+++ lib/io.ts
@@ -1,44 +1,103 @@
 import { parse, stringify } from 'csv/sync';
+import Path from 'path';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 import { ethers } from 'ethers';
-import { SAFE_DEPLOYED_IN_BLOCK } from './const';
+import _ from 'lodash';
 
 const SNAPSHOT_FILENAME = 'snapshot.csv';
 const NEXT_BLOCK_INFO = 'next.json';
 
-type Snapshot = Record<string, ethers.BigNumber>;
+// type Snapshot = Record<string, ethers.BigNumber>;
+type Transaction = {
+  hash: string;
+  sender: string;
+  value: ethers.BigNumber;
+};
+type Snapshot = {
+  currentBlock?: number;
+  transactions: Record<string, Transaction>;
+};
 
-export function readFromSnapshot(): Snapshot {
-  if (!existsSync(SNAPSHOT_FILENAME)) return {};
-
-  const data = parse(readFileSync(SNAPSHOT_FILENAME), { columns: true }) as {
-    sender: string;
-    value: string;
-  }[];
-
-  return data.reduce<Snapshot>((memo, { sender, value }) => {
-    memo[sender] = ethers.BigNumber.from(value);
-    return memo;
-  }, {});
+function serializeTransaction(transaction: Transaction): string {
+  return JSON.stringify({
+    ...transaction,
+    value: transaction.value.toString(),
+  });
 }
 
-export function writeToSnapshot(snapshot: Snapshot) {
-  writeFileSync(
-    SNAPSHOT_FILENAME,
-    stringify(
-      Object.keys(snapshot).map((sender) => ({ sender, value: snapshot[sender].toString() })),
-      {
-        header: true,
-      },
-    ),
-  );
+function deserializeTransaction(transaction: string): Transaction {
+  const data = JSON.parse(transaction);
+  return {
+    ...data,
+    value: ethers.BigNumber.from(data.value),
+  };
 }
 
-export function getNextBlock() {
-  if (!existsSync(NEXT_BLOCK_INFO)) return undefined;
-  return JSON.parse(readFileSync(NEXT_BLOCK_INFO).toString()).next;
+function serializeSnapshot(snapshot: Snapshot): string {
+  return JSON.stringify({
+    ...snapshot,
+    transactions: _.mapValues(snapshot.transactions, serializeTransaction),
+  });
 }
 
-export function setNextBlock(next: number) {
-  writeFileSync(NEXT_BLOCK_INFO, JSON.stringify({ next }));
+function deserializeSnapshot(snapshot: string): Snapshot {
+  const data = JSON.parse(snapshot);
+  return {
+    ...data,
+    transactions: _.mapValues(data.transactions, deserializeTransaction),
+  };
 }
+
+export function snapshotFilename(startBlock: number, endBlock: number, contractAddress: string) {
+  return Path.join(__dirname, '../output', `${contractAddress}-${startBlock}-${endBlock}.json`);
+}
+
+export function readSnapshot(filename: string) {
+  if (!existsSync(filename)) {
+    return {
+      currentBlock: undefined,
+      transactions: {},
+    };
+  }
+
+  return deserializeSnapshot(readFileSync(filename).toString());
+}
+
+export function writeSnapshot(filename: string, snapshot: Snapshot) {
+  writeFileSync(filename, serializeSnapshot(snapshot));
+}
+
+// export function readFromSnapshot(): Snapshot {
+//   if (!existsSync(SNAPSHOT_FILENAME)) return {};
+
+//   const data = parse(readFileSync(SNAPSHOT_FILENAME), { columns: true }) as {
+//     sender: string;
+//     value: string;
+//   }[];
+
+//   return data.reduce<Snapshot>((memo, { sender, value }) => {
+//     memo[sender] = ethers.BigNumber.from(value);
+//     return memo;
+//   }, {});
+// }
+
+// export function writeToSnapshot(snapshot: Snapshot) {
+//   writeFileSync(
+//     SNAPSHOT_FILENAME,
+//     stringify(
+//       Object.keys(snapshot).map((sender) => ({ sender, value: snapshot[sender].toString() })),
+//       {
+//         header: true,
+//       },
+//     ),
+//   );
+// }
+
+// export function getNextBlock() {
+//   if (!existsSync(NEXT_BLOCK_INFO)) return undefined;
+//   return JSON.parse(readFileSync(NEXT_BLOCK_INFO).toString()).next;
+// }
+
+// export function setNextBlock(next: number) {
+//   writeFileSync(NEXT_BLOCK_INFO, JSON.stringify({ next }));
+// }
