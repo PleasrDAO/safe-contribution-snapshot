332131bb617f7a7d7bfe805ac1d74980a61c26ab
Matt Condon <mattgcondon@gmail.com>
2021-12-13 20:29:52 -0400

    feat: additional rules, simpler exclude check

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ const CEX_OVERRIDES: Record<string, string> = {
     '0xb6d84d05b7facFA94FAFB2f40d849B80A3f34FB7',
   // billy_72#4443
   '0xeb05dd124e0cbc16f2352fab345f55d475e51f88752777d2ebf530024edea2d8':
-    '0xbF4B0bcDcC7DC29AB19f89d08b3a139893CdCFE6',
+    '0xa1C00d9B9d0979D58F06B6fc438ea5ff2836c669',
   // vitozhang.eth#8367
   '0x15a8bd82db8ab1f89735b3363450f58026bb1584e7945de7857ca6ea6dac1675':
     '0x89E200fB309dfea8577bAa5aBD9268de00E27F7d',
@@ -34,18 +34,21 @@ const CEX_OVERRIDES: Record<string, string> = {
   // jack005#6109
   '0xfcf6af6008d668951dd7ba1c7fe875df959c1e769efa5ca96d907b60755a43d2':
     '0x42b70Cd9BE0fba250f7838e0b006C1c3560C42D9',
+  // ysjlitt#8853
+  '0x31febc4cd053a28fe0487997ae7d4103f71133a8514c6a2c2f668723c54e9969':
+    '0x77080c88F9aBd8130866345Ef430EE99fAFcA73E',
 };
 
 // Exclude both the failed and successful auction refunds
-const EXCLUDE_TRANSACTIONS: Record<string, boolean> = {
-  '0xcfa584f6d072fd544a6f4e264eaededd4a5d3a0b842b3d3a6ff173f34682a698': true,
-  '0xad60df9a9df45d471f0a49062c9b6651cbc5de69e3964a4487337daa67104c9e': true,
-}
+const EXCLUDE_TRANSACTIONS = new Set([
+  '0xcfa584f6d072fd544a6f4e264eaededd4a5d3a0b842b3d3a6ff173f34682a698',
+  '0xad60df9a9df45d471f0a49062c9b6651cbc5de69e3964a4487337daa67104c9e',
+]);
 
-const KNOWN_MISSING_TRANSACTIONS: Record<string, boolean> = {
-  '0x34c2c9aaafa12c317f4489ec2947650ce4213e3b90e3c2ba2267d2b1281650e9': true,
-  '0x3c39bc7e954109456b03a29c40c653a7e83f092ee4d2d272f34b6646a018cbe1': true,
-}
+const KNOWN_MISSING_TRANSACTIONS = new Set([
+  '0x34c2c9aaafa12c317f4489ec2947650ce4213e3b90e3c2ba2267d2b1281650e9',
+  '0x3c39bc7e954109456b03a29c40c653a7e83f092ee4d2d272f34b6646a018cbe1',
+]);
 
 const SAFE_DEPLOYED_IN_BLOCK = 13724221;
 const AUCTION_ENDED_IN_BLOCK = 13770208;
@@ -120,11 +123,13 @@ async function main() {
       console.log(`Invalid event??`, event);
       return;
     }
-    if (EXCLUDE_TRANSACTIONS[event.transactionHash]) {
+
+    if (EXCLUDE_TRANSACTIONS.has(event.transactionHash)) {
       console.log('Exluding transaction', event.transactionHash);
       return;
     }
-    if (KNOWN_MISSING_TRANSACTIONS[event.transactionHash]) {
+
+    if (KNOWN_MISSING_TRANSACTIONS.has(event.transactionHash)) {
       console.log('!!! FOUND KNOWN MISSING !!!', event.transactionHash);
     }
 
@@ -133,14 +138,14 @@ async function main() {
 
     if (CEX_OVERRIDES[event.transactionHash]) {
       console.log(
-        `remapping tx ${event.transactionHash} for ${ethers.utils.formatEther(
-          value,
-        )} ETH from ${event.args.sender} to ${CEX_OVERRIDES[event.transactionHash]}`,
+        `remapping tx ${event.transactionHash} for ${ethers.utils.formatEther(value)} ETH from ${
+          event.args.sender
+        } to ${CEX_OVERRIDES[event.transactionHash]}`,
       );
     }
 
     handleContribution(sender, value);
-  }
+  };
 
   for (let i = fromBlock; i <= toBlock; i = i + BLOCKS_PER_CHUNK) {
     const fromChunkNumber = i;
